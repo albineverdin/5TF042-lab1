9afa2cc3cb5a2dbbd4f13f7d354e06b91b233ceb
Albin <everdin.albin@gmail.com>
2026-03-27 11:56:03 +0100

    Subscriber response model

---
+++ AdSystem/Models/SubscriberResponse.cs
@@ -0,0 +1,11 @@
+namespace AdSystem.Models;
+
+public class SubscriberResponse
+{
+    public string FirstName { get; set; } = string.Empty;
+    public string LastName { get; set; } = string.Empty;
+    public string PhoneNumber { get; set; } = string.Empty;
+    public string Address { get; set; } = string.Empty;
+    public string PostalCode { get; set; } = string.Empty;
+    public string City { get; set; } = string.Empty;
+}
