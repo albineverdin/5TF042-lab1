312d709767159ac7669453d02ea7a6ecac3f211d
Albin <everdin.albin@gmail.com>
2026-03-27 11:55:51 +0100

    Ad DAL

---
+++ AdSystem/DAL/AdRepository.cs
@@ -0,0 +1,29 @@
+using Microsoft.EntityFrameworkCore;
+using AdSystem.Data;
+using AdSystem.Models;
+
+namespace AdSystem.DAL;
+
+public class AdRepository : IAdRepository
+{
+    private readonly AppDbContext _context;
+
+    public AdRepository(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<IEnumerable<Ad>> GetAllAsync()
+    {
+        return await _context.Ads
+            .Include(a => a.Advertiser)
+            .ToListAsync();
+    }
+
+    public async Task<Ad> CreateAsync(Ad ad)
+    {
+        _context.Ads.Add(ad);
+        await _context.SaveChangesAsync();
+        return ad;
+    }
+}
---
+++ AdSystem/DAL/IAdRepository.cs
@@ -0,0 +1,9 @@
+using AdSystem.Models;
+
+namespace AdSystem.DAL;
+
+public interface IAdRepository
+{
+    Task<IEnumerable<Ad>> GetAllAsync();
+    Task<Ad> CreateAsync(Ad ad);
+}
