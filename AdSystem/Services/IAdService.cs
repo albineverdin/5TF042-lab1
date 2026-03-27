bcafe163284f418f8cbddb946a117670752d2606
Albin <everdin.albin@gmail.com>
2026-03-27 14:41:10 +0100

    misc

--- AdSystem/Services/IAdService.cs
+++ AdSystem/Services/IAdService.cs
@@ -5,6 +5,7 @@ namespace AdSystem.Services;
 public interface IAdService
 {
     Task<SubscriberResponse?> LookupSubscriberAsync(string subscriptionNumber);
+    Task<IEnumerable<Ad>> GetAllAdsAsync();
     Task<Ad> CreateSubscriberAdAsync(string subscriptionNumber, string title, string content, decimal itemPrice);
     Task<Ad> CreateCompanyAdAsync(string organisationNumber, string name, string phone,
         string address, string postalCode, string city,
